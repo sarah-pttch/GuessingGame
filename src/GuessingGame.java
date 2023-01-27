e71d4f6ed47be86a1a5d4974da204c43cb72feed
sarah-pttch <sarah.pttch@gmail.com>
2023-01-27 11:37:41 +0100

    added while loop

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class GuessingGame {
 	
@@ -6,6 +7,21 @@ public class GuessingGame {
 		Random rand = new Random();
 		int number = rand.nextInt(20)+1;
 		
+		Scanner scan = new Scanner(System.in);
 		
+		System.out.println("What number between 1 and 20 am I thinking of?");
+		String input = scan.next();
+		int guess = Integer.parseInt(input);
+		
+		while(guess != number) {
+			if (guess > number) {
+				System.out.println("Too high! Try again!");
+			} else {System.out.println("Too low! Try again!");
+			}
+			input = scan.next();
+			guess = Integer.parseInt(input);
+		}
+		
+		System.out.println("You guessed correctly!");
 	}
 }
