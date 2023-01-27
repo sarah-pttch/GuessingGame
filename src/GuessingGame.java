d922455c513f8570edb153ea9537a9bc3cad6da6
sarah-pttch <sarah.pttch@gmail.com>
2023-01-27 10:31:48 +0100

    start project

---
+++ src/GuessingGame.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+public class GuessingGame {
+	
+	public static void main(String[] args) {
+		Random rand = new Random();
+		int number = rand.nextInt(20)+1;
+		
+		
+	}
+}
